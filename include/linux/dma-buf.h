6321b89ba11160bfc8a3dfa141f2ffaa81868e26
Ashish Mhetre <amhetre@nvidia.com>
2023-04-05 06:49:02 +0000

    dma-buf: defer unmapping for dma shared buffer till free
    BugLink: https://bugs.launchpad.net/bugs/2019240
    
    Implement dma-buf "Deferred unmapping", which stores all attachments and
    mappings until its dmabuf is freed. This can be
    modified to allocate dynamically after some statistics is
    implemented.
    
    This patch allows the exporter specify if the dmabuf it exports can
    use the deferred unmapping. This patch also adds refcounting to the
    dma_buf_attachment to find out any incorrect usage by dmabuf importers.
    
    Also, the exporter can specify if it needs the dmabuf framework to do
    cache sync operations while dma map/unmap occur.
    
    Similarly, devices need to be able to opt for the deferred unmapping.
    This is taken care in a different patch.
    
    For the further optimization, if we want to clean caches under some
    memory pressure(ex: lack of IOVA space), we need to implement the
    following features:
    
      (1) Statistics: Used by Shrinker.
      (2) Shrinker: Reduce cached mappings upon demand
    
    Reviewed-by: Sachin Nikam <snikam@nvidia.com>
    Signed-off-by: Puneet Saxena <puneets@nvidia.com>
    Signed-off-by: Ashish Mhetre <amhetre@nvidia.com>
    Signed-off-by: Hiroshi Doyu <hdoyu@nvidia.com>
    Signed-off-by: Sri Krishna chowdary <schowdary@nvidia.com>
    Signed-off-by: Pritesh Raithatha <praithatha@nvidia.com>
    Reviewed-by: Laxman Dewangan <ldewangan@nvidia.com>
    Reviewed-by: Ankur Pawar <ankurp@nvidia.com>
    Tested-by: Ankur Pawar <ankurp@nvidia.com>
    Signed-off-by: Abhilash G <abhilashg@nvidia.com>
    Acked-by: Brad Figg <bfigg@nvidia.com>
    Acked-by: Jacob Martin <jacob.martin@canonical.com>
    Acked-by: Ian May <ian.may@canonical.com>
    Signed-off-by: Brad Figg <bfigg@nvidia.com>

--- include/linux/dma-buf.h
+++ include/linux/dma-buf.h
@@ -423,6 +423,11 @@ struct dma_buf {
 	 */
 	struct dma_resv *resv;
 
+	/* dma-buf stashing is optimized for host1x context device. Adding flag
+	 * to find out whether dma_buf is attached to any context device or not.
+	 */
+	bool context_dev;
+
 	/** @poll: for userspace poll support */
 	wait_queue_head_t poll;
 
@@ -509,12 +514,16 @@ struct dma_buf_attachment {
 	struct dma_buf *dmabuf;
 	struct device *dev;
 	struct list_head node;
+	/* Adding list node for device attachments. */
+	struct list_head dev_node;
 	struct sg_table *sgt;
 	enum dma_data_direction dir;
 	bool peer2peer;
 	const struct dma_buf_attach_ops *importer_ops;
 	void *importer_priv;
 	void *priv;
+	atomic_t ref;
+	atomic_t maps;
 };
 
 /**
@@ -608,6 +617,8 @@ struct dma_buf *dma_buf_export(const struct dma_buf_export_info *exp_info);
 int dma_buf_fd(struct dma_buf *dmabuf, int flags);
 struct dma_buf *dma_buf_get(int fd);
 void dma_buf_put(struct dma_buf *dmabuf);
+
+void dma_buf_release_stash(struct device *dev);
 
 struct sg_table *dma_buf_map_attachment(struct dma_buf_attachment *,
 					enum dma_data_direction);
