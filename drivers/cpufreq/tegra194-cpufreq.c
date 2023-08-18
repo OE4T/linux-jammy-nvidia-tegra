0b78fb37ffe4e4c3007588eb767ecdb1c29870da
Sumit Gupta <sumitg@nvidia.com>
2023-08-18 14:12:43 +0530

    NVIDIA: SAUCE: driver: cpufreq: remove volatile as not needed
    BugLink: https://bugs.launchpad.net/bugs/2034622
    
    Removing volatile for "delta_refcnt" as its not needed
    and improved comments
    
    http://nvbugs/3791630
    http://nvbugs/3855010
    http://nvbugs/3554228
    
    Signed-off-by: Sumit Gupta <sumitg@nvidia.com>
    Reviewed-by: Bibek Basu <bbasu@nvidia.com>
    Signed-off-by: Laxman Dewangan <ldewangan@nvidia.com>
    Acked-by: Jacob Martin <jacob.martin@canonical.com>
    Acked-by: Noah Wager <noah.wager@canonical.com>
    Signed-off-by: Jacob Martin <jacob.martin@canonical.com>

--- drivers/cpufreq/tegra194-cpufreq.c
+++ drivers/cpufreq/tegra194-cpufreq.c
@@ -5,7 +5,6 @@
 
 #include <linux/cpu.h>
 #include <linux/cpufreq.h>
-#include <linux/delay.h>
 #include <linux/dma-mapping.h>
 #include <linux/module.h>
 #include <linux/of.h>
@@ -155,7 +154,7 @@ static void tegra234_read_counters(struct tegra_cpu_ctr *c)
 {
 	struct tegra194_cpufreq_data *data = cpufreq_get_driver_data();
 	void __iomem *actmon_reg;
-	volatile u32 delta_refcnt;
+	u32 delta_refcnt;
 	int cnt = 0;
 	u64 val;
 
@@ -165,6 +164,11 @@ static void tegra234_read_counters(struct tegra_cpu_ctr *c)
 	val = readq(actmon_reg);
 	c->last_refclk_cnt = upper_32_bits(val);
 	c->last_coreclk_cnt = lower_32_bits(val);
+
+	/*
+	 * The sampling window is based on the minimum number of reference
+	 * clock cycles which is known to give a stable value of CPU frequency.
+	 */
 	do {
 		val = readq(actmon_reg);
 		c->refclk_cnt = upper_32_bits(val);
@@ -245,13 +249,18 @@ static inline u32 map_ndiv_to_freq(struct mrq_cpu_ndiv_limits_response
 static void tegra194_read_counters(struct tegra_cpu_ctr *c)
 {
 	struct tegra194_cpufreq_data *data = cpufreq_get_driver_data();
-	volatile u32 delta_refcnt;
+	u32 delta_refcnt;
 	int cnt = 0;
 	u64 val;
 
 	val = read_freq_feedback();
 	c->last_refclk_cnt = lower_32_bits(val);
 	c->last_coreclk_cnt = upper_32_bits(val);
+
+	/*
+	 * The sampling window is based on the minimum number of reference
+	 * clock cycles which is known to give a stable value of CPU frequency.
+	 */
 	do {
 		val = read_freq_feedback();
 		c->refclk_cnt = lower_32_bits(val);
@@ -323,9 +332,8 @@ static unsigned int tegra194_calculate_speed(u32 cpu)
 	u32 rate_mhz;
 
 	/*
-	 * udelay() is required to reconstruct cpu frequency over an
-	 * observation window. Using workqueue to call udelay() with
-	 * interrupts enabled.
+	 * Reconstruct cpu frequency over an observation/sampling window.
+	 * Using workqueue to keep interrupts enabled during the interval.
 	 */
 	read_counters_work.c.cpu = cpu;
 	INIT_WORK_ONSTACK(&read_counters_work.work, tegra_read_counters);
